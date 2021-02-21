28107c5ae5a9d96d2ba1bd6924771e6da80458de
Maksym Naichuk <mnaichuk@heliostech.fr>
2021-02-21 22:20:20 +0200

    Feature: Webhook daemon

---
+++ app/workers/amqp/webhook_processor.rb
@@ -0,0 +1,96 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module Workers
+  module AMQP
+    class WebhookProcessor < Base
+      def process(payload)
+        if payload[:event] == 'deposit'
+          process_deposit_event(payload)
+        elsif payload[:event] == 'withdraw'
+          process_withdraw_event(payload)
+        end
+      end
+
+      def process_deposit_event(payload)
+        # For deposit events we use only Deposit wallets.
+        Wallet.where(status: :active, kind: :deposit, gateway: payload[:adapter]).each do |w|
+          service = w.service
+          next unless service.adapter.respond_to?(:trigger_webhook_event)
+
+          transactions = service.trigger_webhook_event(payload)
+          next unless event.present?
+          accepted_deposits = []
+          ActiveRecord::Base.transaction do
+            accepted_deposits = process_deposit(transactions)
+          end
+          accepted_deposits.each(&:process!) if accepted_deposits.present?
+        end
+      end
+
+      def process_withdraw_event(payload)
+        # For withdraw events we use only Withdraw events.
+        Wallet.where(status: :active, kind: :hot, gateway: payload[:adapter]).each do |w|
+          service = w.service
+          next unless service.adapter.respond_to?(:trigger_webhook_event)
+
+          transactions = service.trigger_webhook_event(payload)
+          next unless event.present?
+
+          ActiveRecord::Base.transaction do
+            update_withdrawal(transactions) if transactions.present?
+          end
+        end
+      end
+
+      def process_deposit(transactions)
+        accepted_deposits = find_or_create_deposit!(transactions)
+
+        accepted_deposits.compact if accepted_deposits.present?
+      end
+
+      def find_or_create_deposit!(transactions)
+        transactions.map do |transaction|
+          payment_address = PaymentAddress.find_by(wallet: Wallet.deposit_wallet(transaction.currency_id), address: transaction.to_address)
+          next if payment_address.blank?
+
+          Rails.logger.info { "Deposit transaction detected: #{transaction.inspect}" }
+          deposit =
+            Deposits::Coin.find_or_create_by!(
+              currency_id: transaction.currency_id,
+              txid: transaction.hash,
+              txout: transaction.txout
+            ) do |d|
+              d.address = transaction.to_address
+              d.amount = transaction.amount
+              d.member = payment_address.member
+              d.block_number = transaction.block_number
+            end
+          # TODO: check if block number changed.
+          next unless transaction.status.success?
+          deposit.accept!
+          deposit
+        end
+      end
+
+      def update_withdrawal(transactions)
+        transactions.each do |transaction|
+          withdrawal = Withdraws::Coin.confirming
+                                      .find_by(currency_id: transaction.currency_id, txid: transaction.hash)
+
+          if withdrawal.blank?
+            Rails.logger.info { "Skipped withdrawal: #{transaction.hash}." }
+            next
+          end
+
+          Rails.logger.info { "Withdraw transaction detected: #{transaction.inspect}" }
+          if transaction.status.failed?
+            withdrawal.fail!
+          elsif transaction.status.success?
+            withdrawal.success!
+          end
+        end
+      end
+    end
+  end
+end
